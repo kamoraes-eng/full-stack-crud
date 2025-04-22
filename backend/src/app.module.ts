265b7a55cbe2d96b1404ab04be32a26e6a21d7b9
kamoraes.eng <kaique.amoraes@icloud.com>
2025-04-22 12:49:50 -0300

    appmodule ddd

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,14 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { UsersModule } from './modules/users/users.module'
+import { typeOrmConfig } from './shared/infrastructure/typeorm.config'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot(typeOrmConfig),
+    UsersModule,
+  ],
 })
 export class AppModule {}
