606fd2356c68eaf638a8c16b5e51b07f8740bb53
kamoraes.eng <kaique.amoraes@icloud.com>
2025-04-22 13:36:40 -0300

    docker port issue with postgress resolved

--- backend/src/shared/infrastructure/typeorm.config.ts
+++ backend/src/shared/infrastructure/typeorm.config.ts
@@ -6,18 +6,11 @@ config();
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '5432'), 
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
-  database: process.env.DB_NAME || 'app_db',
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: 'postgres',  
+  password: 'postgres',  
+  database: 'user_crud',
   entities: [__dirname + '/../../modules/**/*.orm-entity.{ts,js}'],
-  synchronize: process.env.NODE_ENV !== 'production', 
+  synchronize: process.env.NODE_ENV !== 'production',
   logging: process.env.NODE_ENV === 'development',
-  migrations: [__dirname + '/../migrations/*{.ts,.js}'],
-  migrationsRun: true,
-  extra: {
-    ssl: process.env.DB_SSL === 'true' ? { 
-      rejectUnauthorized: false 
-    } : false,
-  }
 };
