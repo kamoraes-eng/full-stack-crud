6cbcc188559408ad3248831c4c32c2c9588de990
kamoraes.eng <kaique.amoraes@icloud.com>
2025-04-22 12:51:55 -0300

    typeorm config, added after build success test.

--- backend/src/shared/infrastructure/typeorm.config.ts
+++ backend/src/shared/infrastructure/typeorm.config.ts
@@ -1,13 +1,23 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { config } from 'dotenv';
+
+config(); 
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  entities: [__dirname + '/../../**/*.entity{.ts,.js}'],
-  synchronize: process.env.NODE_ENV !== 'production',
-  logging: true
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || '5432'), 
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
+  database: process.env.DB_NAME || 'app_db',
+  entities: [__dirname + '/../../modules/**/*.orm-entity.{ts,js}'],
+  synchronize: process.env.NODE_ENV !== 'production', 
+  logging: process.env.NODE_ENV === 'development',
+  migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+  migrationsRun: true,
+  extra: {
+    ssl: process.env.DB_SSL === 'true' ? { 
+      rejectUnauthorized: false 
+    } : false,
+  }
 };
